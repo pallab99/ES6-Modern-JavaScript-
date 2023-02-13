0bcd56559c9b09884b114e16d4fd883c4509b7a9
Pallab Majumdar <kitkatpallab@gmail.com>
2023-02-14 00:33:27 +0600

    Arrow Function

---
+++ Arrow_function.js
@@ -0,0 +1,15 @@
+function sum(a, b) {
+  return a + b;
+}
+
+let s = sum(2, 3);
+console.log(s);
+
+let add = (a, b) => {
+  return a + b;
+};
+
+console.log(add(3, 4));
+
+let double = (a) => a * 2;
+console.log(double(2));
